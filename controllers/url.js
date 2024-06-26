6a539cf98bf6733ca8ab35ab851e18ef4d1010e6
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2024-06-26 13:09:24 +0530

    url frontHome page

--- controllers/url.js
+++ controllers/url.js
@@ -3,29 +3,30 @@ const shortid = require("shortid");
 const URL = require("../models/url");
 
 async function handleGenerateNewShortUrl(req, res) {
-    const body = req.body;
-    if (!body.url) return res.status(400).json({ error: "URL is required" });
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "URL is required" });
 
-    const shortID = shortid(8);
-    await URL.create({
-        shortId: shortID,
-        redirectURL: body.url,
-        visitHistory: [],
-    })
-    
+  const shortID = shortid(8);
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
 
-    res.json({ id: shortID });
-}
-
-async function handleGetAnalyticsUrl(req, res){
-    const shortId = req.params.shortId;
-    const result =await URL.findOne({shortId})
-    return res.json({totalClicks: result.visitHistory.length, analytics : result.visitHistory});
-
+  return res.render("home", { id: shortID });
 
 }
 
-module.exports ={
-    handleGenerateNewShortUrl,
-    handleGetAnalyticsUrl,
+async function handleGetAnalyticsUrl(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
+
+module.exports = {
+  handleGenerateNewShortUrl,
+  handleGetAnalyticsUrl,
+};
--- index.js
+++ index.js
@@ -2,18 +2,32 @@ const express = require("express");
 const userUrlRoutes = require("./routes/url");
 const {connectMongoDb} = require("./connect");
 const URL = require("./models/url");
+ const path  = require('path');
+ const staticRoute = require('./routes/staticRouter');
 
 const app = express();
 const PORT = 8001;
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use("/url", userUrlRoutes);
+app.use("/", staticRoute);
 
 connectMongoDb("mongodb://127.0.0.1:27017/short-url")
   .then(() => console.log("mongoDb connected"))
   .catch((err) => console.log("mongo error", err));
 
-app.get('/:shortId', async (req, res) =>{
+
+  app.set('view engine', 'ejs');
+  app.set('views', path.resolve('./views'));
+
+//   app.get('/test', async (req, res) => {
+//     const allUrl = await URL.find({});
+//     return res.render("home", {urls: allUrl})
+
+//   })
+
+app.get('/url/:shortId', async (req, res) =>{
     const shortId = req.params.shortId;
     const entry =await URL.findOneAndUpdate({
         shortId
